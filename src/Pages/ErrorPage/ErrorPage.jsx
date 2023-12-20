2a3f8d119e17390e218824954611fd360128df4a
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2023-12-21 01:10:21 +0600

    error page with image added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,18 @@
-
+import { Link } from "react-router-dom";
+import errorImage from "../../../public/preview.gif"
 
 const ErrorPage = () => {
     return (
         <div>
-            
-        </div>
+      <div className="flex justify-center items-center mt-24">
+        <img className="h-[400px]" src={errorImage} alt="" />
+      </div>
+      <div className="flex justify-center items-center mt-12">
+        <Link to="/">
+          <button className="btn bg-sky-600 text-white">Back To Home</button>
+        </Link>
+      </div>
+    </div>
     );
 };
 
