e66c72ff6cda739a9dfd86e82f16a149d79a77a2
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2023-12-21 19:57:07 +0600

    task added

--- src/Pages/Dashboard/TaskCreate/TaskCreate.jsx
+++ src/Pages/Dashboard/TaskCreate/TaskCreate.jsx
@@ -1,45 +1,46 @@
-import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
-import { AuthContext } from "../../../Provider/AuthProvider";
 import { Helmet } from "react-helmet-async";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { useState } from "react";
 
 const TaskCreate = () => {
-
-    const { user, loading } = useContext(AuthContext);
-  // console.log(user, loading);
-  const { email, displayName } = user || "";
 
   const { register, handleSubmit } = useForm();
   const currentDate = new Date();
   const [selectedDeadline, setSelectedDeadline] = useState(new Date());
+  const navigate = useNavigate()
 
-//   const onSubmit = (data) => {
-//     data.userEmail = email;
-//     data.userName = displayName;
+  const onSubmit = (data) => {
 
-//     data.deadline = selectedDeadline;
-//     // console.log(data.deadline)
+    const formatDeadline = selectedDeadline.toLocaleDateString()
+    data.deadline = formatDeadline
+    data.status = 'todo';
+    fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
 
-//     fetch("https://job-seeking-server-virid.vercel.app/addajobs", {
-//       method: "POST",
-//       headers: {
-//         "content-type": "application/json",
-//       },
-//       body: JSON.stringify(data),
-//     })
-//       .then((res) => res.json())
-
-//       .then((result) => {
+      .then((result) => {
       
-//         toast.success("successfully job added");
-//         // console.log(result);
-//         navigate("/");
+        toast.success("successfully task added");
+        console.log(result);
+        navigate("/");
         
-//       });
-//   };
+      });
+  };
+
+  const handleDeadline = (date) => {
+    setSelectedDeadline(date);
+  };
 
     return (
         <div>
@@ -50,7 +51,7 @@ const TaskCreate = () => {
       <h2 className="text-2xl md:text-3xl font-bold my-2 text-center text-cyan-600">
         Add A Task
       </h2>
-      <form className="flex flex-col gap-3">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
 
         <label
           htmlFor=""
@@ -108,6 +109,7 @@ const TaskCreate = () => {
         <div className="w-full">
           <DatePicker
             selected={selectedDeadline}
+            onChange={handleDeadline}
             dateFormat="yyyy/MM/dd"
             className="input input-bordered text-input bg-gray-300 px-5 py-2 rounded w-full max-w-xs"
             minDate={currentDate}
@@ -123,6 +125,7 @@ const TaskCreate = () => {
         />
       </form>
     </div>
+    <ToastContainer/>
         </div>
     );
 };
